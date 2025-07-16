0d6663e76cf5672f1c1150bbabed88b39897aa8c
AlexSalavat <alex.salavat88@gmail.com>
2025-07-16 11:28:59 +0300

    product card: sh

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 import "./ProductCard.css";
 
-export default function ProductCard({ image, name, country, onMore }) {
+export default function ProductCard({ image, name, onMore }) {
   return (
     <div className="product-card">
       <img src={image} alt={name} className="product-img" />
       <div className="product-card-content">
         <div className="product-title">{name}</div>
-        <div className="product-country">{country}</div>
         <button className="product-more-btn" onClick={onMore}>
           Подробнее
         </button>
