20bfd18b6ffae23fadaa1b10a03130d373b319aa
AlexSalavat <alex.salavat88@gmail.com>
2025-07-16 20:36:38 +0300

    first djjj

--- src/components/CategoryCard.jsx
+++ src/components/CategoryCard.jsx
@@ -4,12 +4,8 @@ import "./CategoryCard.css";
 export default function CategoryCard({ image, name, onClick }) {
   return (
     <div className="category-card" onClick={onClick} tabIndex={0} role="button">
-      <div className="category-card-img-wrap">
-        <img src={image} alt={name} className="category-card-img" />
-        <div className="category-card-title">
-          {name}
-        </div>
-      </div>
+      <img src={image} alt={name} className="category-card-img" />
+      <div className="category-card-title">{name}</div>
     </div>
   );
 }
