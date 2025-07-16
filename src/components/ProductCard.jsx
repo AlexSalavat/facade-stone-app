f7ff4f9d4ed85200b5f19367cb5414cc24cbff0b
AlexSalavat <alex.salavat88@gmail.com>
2025-07-16 11:37:19 +0300

    product card: shр

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -4,7 +4,9 @@ import "./ProductCard.css";
 export default function ProductCard({ image, name, onMore }) {
   return (
     <div className="product-card">
-      <img src={image} alt={name} className="product-img" />
+      <div className="product-img-wrap">
+        <img src={image} alt={name} className="product-img" />
+      </div>
       <div className="product-card-content">
         <div className="product-title">{name}</div>
         <button className="product-more-btn" onClick={onMore}>
