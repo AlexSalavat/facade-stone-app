154dd062c5e01c87766ad50d15e99f8f3fe11fb5
AlexSalavat <alex.salavat88@gmail.com>
2025-07-21 16:01:21 +0300

    Обновлены описания т

--- src/components/products/ProductCard.jsx
+++ src/components/products/ProductCard.jsx
@@ -1,37 +1,14 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import "./ProductsGrid.css";
 
 const ProductCard = ({ product }) => {
   const navigate = useNavigate();
 
   return (
-    <div
-      className="product-card"
-      onClick={() => navigate(`/product/${product.id}`)}
-      style={{
-        backgroundColor: "#1a1a1a",
-        borderRadius: "12px",
-        padding: "12px",
-        textAlign: "center",
-        width: "100%",
-        maxWidth: "170px",
-        margin: "auto",
-        color: "#fff",
-        cursor: "pointer"
-      }}
-    >
-      <img
-        src={product.images[0]}
-        alt={product.name}
-        style={{
-          width: "100%",
-          height: "160px",
-          objectFit: "cover",
-          borderRadius: "8px",
-          marginBottom: "8px"
-        }}
-      />
-      <div style={{ fontSize: "16px", fontWeight: "500" }}>{product.name}</div>
+    <div className="product-card" onClick={() => navigate(`/product/${product.id}`)}>
+      <img src={product.images[0]} alt={product.name} />
+      <h3>{product.name}</h3>
     </div>
   );
 };
