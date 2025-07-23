cb83d6d5e128e6802f075910dabfa909cde797c5
AlexSalavat <alex.salavat88@gmail.com>
2025-07-23 15:27:42 +0300

    deploy: обновил карточки, добавил картинк

--- src/components/news/NewsGrid.jsx
+++ src/components/news/NewsGrid.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "../../styles/InfoGrid.css";
 import BackButton from "../BackButton";
-import { news } from "../../data/news.js";
+import { news } from "../../data/news";
 
 const NewsGrid = () => (
   <div>
@@ -10,10 +10,7 @@ const NewsGrid = () => (
     <div className="info-grid">
       {news.map((item, idx) => (
         <div className="info-card" key={idx}>
-          {/* Показываем иконку, если она есть */}
-          {item.icon && <div className="info-card-icon">{item.icon}</div>}
-          {/* Если используешь картинки — можешь добавить сюда */}
-          {item.image && <img src={item.image} alt={item.title} className="info-card-img" />}
+          <img src={item.image} alt={item.title} className="info-card-img" />
           <div className="info-card-title">{item.title}</div>
           <div className="info-card-content">{item.content}</div>
         </div>
--- src/components/useful/UsefulGrid.jsx
+++ src/components/useful/UsefulGrid.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "../../styles/InfoGrid.css";
 import BackButton from "../BackButton";
-import { useful } from "../../data/useful.js";
+import { useful } from "../../data/useful";
 
 const UsefulGrid = () => (
   <div>
@@ -10,9 +10,7 @@ const UsefulGrid = () => (
     <div className="info-grid">
       {useful.map((item, idx) => (
         <div className="info-card" key={idx}>
-          {/* Показываем иконку, если она есть */}
-          {item.icon && <div className="info-card-icon">{item.icon}</div>}
-          {item.image && <img src={item.image} alt={item.title} className="info-card-img" />}
+          <img src={item.image} alt={item.title} className="info-card-img" />
           <div className="info-card-title">{item.title}</div>
           <div className="info-card-content">{item.content}</div>
         </div>
