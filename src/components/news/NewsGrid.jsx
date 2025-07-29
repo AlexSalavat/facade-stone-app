58ae56c3e38b5cc1d71d2198d0b68379cbc310ff
AlexSalavat <alex.salavat88@gmail.com>
2025-07-29 18:24:47 +0300

    fix: co

--- src/components/news/NewsGrid.jsx
+++ src/components/news/NewsGrid.jsx
@@ -7,14 +7,16 @@ const NewsGrid = () => (
   <div>
     <BackButton />
     <h2 className="news-title-main">Новости</h2>
-    <div className="news-grid">
+    <div className="news-cards-grid">
       {news.map(item => (
-        <div className="news-card" key={item.id}>
-          <div className="news-card-image-wrap">
-            <img src={item.image} alt={item.title} className="news-card-image" />
+        <div className="news-card-simple" key={item.id}>
+          <div className="news-card-imgwrap">
+            <img src={item.image} alt={item.title} className="news-card-img" />
           </div>
-          <div className="news-card-title">{item.title}</div>
-          <div className="news-card-content">{item.content}</div>
+          <div className="news-card-texts">
+            <div className="news-card-title">{item.title}</div>
+            <div className="news-card-content">{item.content}</div>
+          </div>
         </div>
       ))}
     </div>
