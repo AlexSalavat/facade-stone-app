4132580e60d021432bbf1dc088934a8797bf0ac6
AlexSalavat <alex.salavat88@gmail.com>
2025-07-29 16:03:23 +0300

    fix: correct bg

--- src/components/categories/CategoryCard.jsx
+++ src/components/categories/CategoryCard.jsx
@@ -1,16 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import '../../styles/CategoryCard.css';
+import "../../styles/CategoryCard.css";
 
-const CategoryCard = ({ name, image, to }) => {
-  return (
-    <Link to={to} className="category-card">
-      <div className="category-card-image-wrap">
-        <img src={image} alt={name} className="category-card-image" />
-      </div>
-      <div className="category-name">{name}</div>
-    </Link>
-  );
-};
+const CategoryCard = ({ name, image, to }) => (
+  <Link to={to} className="category-card-link">
+    <div className="category-card-img-wrap">
+      <img src={image} alt={name} className="category-card-img" />
+    </div>
+    <div className="category-card-title">{name}</div>
+  </Link>
+);
 
 export default CategoryCard;
