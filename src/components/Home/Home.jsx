2ad49e1cb6292deb2d3ef52f709fbc0140b8fc11
AlexSalavat <alex.salavat88@gmail.com>
2025-08-02 17:23:32 +0300

    update main page and i

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -6,11 +6,13 @@ export default function Home() {
       <div className="home-bg" />
       <div className="home-overlay">
         <h1 className="home-title">KERAMOGROUP</h1>
-        <p className="home-desc">
-          ДОМ С ХАРАКТЕРОМ.<br />
-          Фасадный камень для тех, кто выбирает стиль и мощь.<br /><br />
-          <span className="home-accent">Выделяйся из толпы. Примерь свой новый фасад прямо сейчас!</span>
-        </p>
+        <div className="home-subtitle">
+          Искусственный камень для фасадов.<br />
+          Эстетика. Прочность. Вечный стиль.
+        </div>
+        <div className="home-bottom">
+          Примерь облицовку онлайн. Сделай дом заметнее всех.
+        </div>
       </div>
     </div>
   );
