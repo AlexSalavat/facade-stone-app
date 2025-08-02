227b4471f1ce6d48f73bd27db14e185f114e34bd
AlexSalavat <alex.salavat88@gmail.com>
2025-08-02 18:09:29 +0300

    update main page a

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./components/Home/Home";
 import CategoryView from "./components/categories/CategoryView";
 import ProductPage from "./components/products/ProductPage";
-import NewsGrid from "./components/news/NewsGrid";
+import FacadePreview from "./components/FacadePreview"; // Импорт твоей примерки!
 import UsefulGrid from "./components/useful/UsefulGrid";
 import CartPage from "./components/CartPage";
 import BottomNav from "./components/shared/BottomNav";
@@ -15,7 +15,7 @@ export default function App() {
         <Route path="/catalog" element={<CategoryView />} />
         <Route path="/catalog/:category" element={<CategoryView />} />
         <Route path="/product/:productId" element={<ProductPage />} />
-        <Route path="/news" element={<NewsGrid />} />
+        <Route path="/preview" element={<FacadePreview />} />    {/* Примерка фасада */}
         <Route path="/useful" element={<UsefulGrid />} />
         <Route path="/cart" element={<CartPage />} />
       </Routes>
